360bcf43318ad7b7efe517e912995f0af942f948
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2023-01-19 00:28:05 +0900

    backjun 1065 한수
    브루트포스

---
+++ backjun/src/b1065/Main.java
@@ -0,0 +1,28 @@
+package b1065;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		if(num >= 1 && num < 100) System.out.println(num);
+		else {
+			int count = 99;
+			int cur = 100;
+			for(;cur <= num;cur++) {
+				if(cur != 1000) {
+					int one = cur % 10;
+					int two = (cur % 100) / 10;
+					int three = (cur % 1000) / 100;
+					
+					if((three - two) == (two - one)) count += 1;
+				}
+			}
+			System.out.println(count);
+		}
+		sc.close();
+	}
+}
