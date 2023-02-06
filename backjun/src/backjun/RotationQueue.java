16b6c101ae87b6f9a14ce06c04a29f301ed20411
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2023-02-07 01:40:43 +0900

    23.02.07 백준 1021 회전하는 큐
    힌트 넘봤자너~

---
+++ backjun/src/backjun/RotationQueue.java
@@ -0,0 +1,60 @@
+package backjun;
+
+import java.io.*;
+import java.util.*;
+
+// 백준 1021 회전하는 큐
+public class RotationQueue {
+	static int N;
+	static int M;
+	static int num[];
+	static int res;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str[] = br.readLine().split(" ");
+		
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
+		
+		str = br.readLine().split(" ");
+		Deque<Integer> deq = new ArrayDeque<>();
+		for(int i = 1; i <= N;i++) deq.add(i);
+		res = 0;
+		
+		for(int i = 0;i < str.length;i++) {
+			int target = Integer.parseInt(str[i]);
+			Iterator<Integer> iter = deq.iterator();
+			int tar_idx = 0;
+			while(iter.hasNext()) {
+				if(iter.next() != Integer.valueOf(target)) tar_idx++;
+				else break;
+			}
+			int half;
+			if(deq.size() % 2 == 0) {
+				half = deq.size() / 2 - 1;
+			}
+			else {
+				half = deq.size() / 2;
+			}
+			
+			int cnt = 0;
+			if(tar_idx <= half) {
+				for(int j = 0;j < tar_idx;j++) {
+					deq.addLast(deq.pollFirst());
+					cnt++;
+				}
+			}
+			else {
+				for(int j = 0; j < deq.size() - tar_idx;j++) {
+					deq.addFirst(deq.pollLast());
+					cnt++;
+				}
+			}
+			
+			deq.pollFirst();
+			res += cnt;
+		}
+		System.out.println(res);
+	}
+}
