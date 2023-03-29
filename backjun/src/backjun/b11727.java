040e62a86c11298fdb499f0749be63172d0414d5
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2023-03-29 18:51:47 +0900

    23.03.29 백준 11727 2xn 타일링 2

---
+++ backjun/src/backjun/b11727.java
@@ -0,0 +1,26 @@
+package backjun;
+
+import java.io.*;
+
+//백준 11727 2xn 타일링 2
+public class b11727 {
+	static int store[];
+	public static int dp(int n) {
+		if(n == 1) return store[n] = 1;
+		if(n == 2) return store[n] = 3;
+		if(store[n] == 0) {
+			store[n] = (dp(n - 1) % 10007 + (2 * dp(n - 2) % 10007)) % 10007;
+		}
+		return store[n];
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		store = new int[N + 1];
+		dp(N);
+		
+		System.out.println(store[N]);
+	}
+}
