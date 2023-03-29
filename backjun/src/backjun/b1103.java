19e21a0721c45df9bfe254e75abf065dfd86805c
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2023-03-30 00:36:12 +0900

    23.03.30 백준 1103 게임 주석 수정

--- backjun/src/backjun/b1103.java
+++ backjun/src/backjun/b1103.java
@@ -18,12 +18,12 @@ public class b1103 {
 	public static int dp(int x, int y) {
 		if(x >= 0 && x < N && y >= 0 && y < M) {
 			if(visited[x][y] == -1) {
-				int cal = 0;	// ��� Ž���� �ϰ� ��� �� �ִ�� ���� �̵��� Ƚ��
+				int cal = 0;	// 사방 탐색을 하고 사방 중 최대로 많이 이동한 횟수
 				if(board[x][y] == -1) {
 					return 0;
 				}
 				else {
-					visited[x][y] = 0;	// �ϴ� �湮 ó��
+					visited[x][y] = 0;	// 일단 방문 처리
 					
 					for(int i = 0;i < 4;i++) {
 						int nx = x + dx[i] * board[x][y];
@@ -34,11 +34,11 @@ public class b1103 {
 					return visited[x][y] = cal;
 				}
 			} else if(visited[x][y] == 0) {
-				// visited == 0�� ��츸 ����Ŭ�̶�� ����� ����
-				// visited == 0�� �ڸ��� : ���� Ž�� ���� ���
-				// visited != 0�� �ڸ��� : Ž���� ����, ��� �� �� �̻� ������ �� �ִ� �κ� X
-				// a -> b�� ���µ� b�� 0�� �ƴ� �ٸ� ���� ���, b -> a�� �� �� �ִٰ� Ȯ���� �� ����.
-				// �װ� �����ߴٸ� a -> b�� ���� ��, b == 0 �̾��� ��.
+				// visited == 0인 경우만 사이클이라고 여기는 이유
+				// visited == 0인 자리들 : 현재 탐색 중인 경로
+				// visited != 0인 자리들 : 탐색이 종료, 사방 중 더 이상 진행할 수 있는 부분 X
+				// a -> b로 갔는데 b가 0이 아닌 다른 수인 경우, b -> a로 올 수 있다고 확신할 수 없음.
+				// 그게 가능했다면 a -> b로 갔을 때, b == 0 이었을 것.
 				cycle = true;
 			} else {
 				return visited[x][y];
@@ -57,7 +57,7 @@ public class b1103 {
 		visited = new int[N][M];
 		for(int i = 0;i < N;i++) Arrays.fill(visited[i], -1);
 		
-		// H : -1�� ǥ��
+		// H : -1로 표시
 		for(int i = 0;i < N;i++) {
 			str = br.readLine().split("");
 			for(int j = 0;j < str.length;j++) {
