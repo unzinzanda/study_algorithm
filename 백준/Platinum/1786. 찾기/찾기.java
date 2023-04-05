1e421341ddfdf898176c35864507c3d503143041
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2023-04-05 14:04:29 +0900

    [Platinum V] Title: 찾기, Time: 3100 ms, Memory: 93280 KB -BaekjoonHub

---
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+//백준 1786 찾기
+/*
+ * KMP 알고리즘을 사용하여 문자열 매칭
+ * */
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char T[] = br.readLine().toCharArray();
+		char P[] = br.readLine().toCharArray();
+		
+		int pi[] = new int[P.length];
+			
+		//부분 문자열 배열 만들기
+		int j = 0;
+		for(int i = 1;i < P.length;i++) {
+			while(j > 0 && P[i] != P[j]) j = pi[j - 1];
+			
+			if(P[i] == P[j]) pi[i] = ++j;
+			else pi[i] = 0;
+		}
+		
+		j = 0;
+		int cnt = 0;
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i = 0;i < T.length;i++) {
+			while(j > 0 && T[i] != P[j]) j = pi[j - 1];
+			if(T[i] == P[j]){
+				if(j == P.length - 1) {
+					cnt++;
+					list.add(i - j + 1);
+					j = pi[j];
+				}
+				else j++;
+			}
+		}
+		
+		System.out.println(cnt);
+		for(int a : list) System.out.print(a + " ");
+	}
+}
