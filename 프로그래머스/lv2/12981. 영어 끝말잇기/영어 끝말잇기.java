c58bd81b208b23ccc510274458a1357dedbce9ce
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2023-04-13 16:40:31 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.54 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        ArrayList<String> used = new ArrayList<>();
+        
+        int i = 1;
+        int turn = 1;
+        char last = words[0].charAt(words[0].length() - 1);
+        used.add(words[0]);
+        for(;i < words.length;i++) {
+            if(used.contains(words[i])) break;
+            else {
+                if(words[i].charAt(0) != last) break;
+                else { 
+                    last = words[i].charAt(words[i].length() - 1);
+                    used.add(words[i]);
+                }
+                if(i % n == n - 1) turn++;
+            }
+        }
+        
+        System.out.println(i);
+        int [] answer = new int[2];
+        if(i == words.length) {
+            answer[0] = 0;
+            answer[1] = 0;
+        }
+        else {
+            answer[0] = i % n + 1;
+            answer[1] = turn;
+        }
+        return answer;
+    }
+}
