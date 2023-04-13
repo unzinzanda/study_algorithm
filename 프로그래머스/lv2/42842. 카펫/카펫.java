e971622865e4ca55d122b3a5931f822ef3c4e351
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2023-04-13 17:42:12 +0900

    Update 카펫.java

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -3,14 +3,14 @@ class Solution {
         int[] answer = new int[2];
         
         int w = 3;
-        int total = brown + yellow;
+        int total = brown + yellow; // 타일이 딱 이만큼 있어야 정답일 가능성 O
         
         find:
         while(true) {
             for(int h = 3;h <= w;h++) {
                 if(h * w != total) continue;
                 else {
-                    int sum = 2 * w + ((h - 2) * 2);
+                    int sum = 2 * w + ((h - 2) * 2);    // 가로 길이 + 가로 길이 + (세로에서 위아래 두 칸 뺀 거) * 2 (중복으로 더할 꺼 빼는 것)
                     if(total - sum == yellow) {
                         answer[0] = w;
                         answer[1] = h;
