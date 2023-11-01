832dcd4f23814f220699bcda657bf49b2cc4e641
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2023-11-02 00:32:22 +0900

    [level 2] Title: 문자열 압축, Time: 0.04 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -2,28 +2,39 @@ class Solution {
     public int solution(String s) {
         int answer = Integer.MAX_VALUE;
         
+        // 문자열의 길이가 1인 경우는 바로 1 리턴
         if(s.length() == 1) return 1;
         
+        // 자를 문자열의 길이를 늘려가며 진행
         for(int i = 1;i < s.length();i++) {
             int ans = 0, subStrCnt = 1;
             String cur = "";
             
             for(int j = 0;j < s.length();j += i) {
+                // 남은 문자열의 길이가 자를 문자열의 길이보다 짧은 경우,
+                // 남은 문자열의 길이를 바로 더해버림.
                 if(j + i > s.length()) {
                     String temp = s.substring(j, s.length());
                     ans += temp.length();
                 }
                 else {
                     String temp = s.substring(j, j + i);
+                    // 처음 시작
                     if(cur.equals("")) {
                         cur = temp;
                     }
+                    // 반복되는 문자열인 경우
                     else if(cur.equals(temp)) {
                         subStrCnt++;
-                    } else if (!cur.equals(temp)) {
+                    }
+                    // 반복되지 않는 문자열인 경우
+                    else if (!cur.equals(temp)) {
+                        // 반복이 2번 이상된 경우, 추가로 반복된 횟수를 더해야 함
+                        // 리턴값이 최종 문자열의 길이이기 때문에 subStrCnt을 문자열로 바꾼 길이를 ans에 더함
                         if(subStrCnt > 1) {
                             ans += Integer.toString(subStrCnt).length();
                         }
+                        // cur의 길이를 더해야 함
                         ans += i;
                         cur = temp;
                         subStrCnt = 1;
