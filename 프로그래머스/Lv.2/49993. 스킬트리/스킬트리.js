7be0c10bf3eea9fbae4b03defcbdbdc8bf892ea8
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2024-01-12 23:29:50 +0900

    Update 스킬트리.js

--- 프로그래머스/Lv.2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/Lv.2/49993. 스킬트리/스킬트리.js
@@ -1,6 +1,10 @@
 /*
     skill_trees에서 skill에 포함되지 않는 스킬은 제거하고 비교
     (아이디어 얻어옴 ㅎㅎ..)
+
+    주의!
+    선행 스킬이 있기 때문에 무조건 skill[0]으로 시작하는 result만 skill과 같은지 비교해도 됨
+    만약 skill에 포함된 skill을 하나도 배우지 않는 스킬트리라면 그것도 가능한 경우가 됨!
 */
 
 function solution(skill, skill_trees) {
