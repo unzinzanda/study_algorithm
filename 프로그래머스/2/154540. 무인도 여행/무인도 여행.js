3c5716e30eb01cde41be903559e4a420ebf62e15
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2024-01-23 19:28:21 +0900

    [level 2] Title: 무인도 여행, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -0,0 +1,33 @@
+function solution(maps) {
+    var answer = [];
+    const dx = [1, -1, 0, 0], dy = [0, 0, 1, -1];
+    const visited = Array.from(new Array(maps.length), () => new Array(maps[0].length).fill(false));
+    
+    let feed = 0;
+    
+    function dfs(x, y) {
+        visited[x][y] = true;
+        feed += Number(maps[x][y]);
+        
+        for(let i = 0;i < 4;i++) {
+            const nx = x + dx[i];
+            const ny = y + dy[i];
+            
+            if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length || visited[nx][ny] || maps[nx][ny] === 'X') continue;
+            
+            dfs(nx, ny);
+        }
+    }
+    
+    for(let i = 0;i < maps.length;i++) {
+        for(let j = 0;j < maps[0].length;j++) {
+            if(!visited[i][j] && maps[i][j] !== 'X') {
+                feed = 0;
+                dfs(i, j);
+                answer.push(feed);
+            }
+        }
+    }
+    
+    return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
+}
