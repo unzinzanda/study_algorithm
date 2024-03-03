7cac0e33efe4e286ce5868ebfbd5e9822e620032
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2024-03-04 00:47:53 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 412 ms, Memory: 10640 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.js
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.js
@@ -1,31 +1,45 @@
-const input = require('fs').readFileSync('./dev/stdin').toString().split('\n').map(v => v.split(' ').map(Number));
-const N = input.shift()[0];
-let answer = 0;
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n").map(el => el.split(" ").map(Number));
 
+/**
+ * 1. 가장 왼쪽의 계란을 든다.
+ * 2. 나머지 계란 중 아무거나 하나 친다. -> 계란이 깨지거나 내구도가 달라진다.
+ * 3. 계란을 내려놓고 오른쪽 계란을 든 후, 2번 과정을 다시 진행한다.
+ * 4. 내려놓은 계란이 가장 오른쪽 계란이거나 모든 계란이 깨졌다면 종료된다.
+ * 
+ * 완탐이니?!?!?!
+ */
+
+const totalEgg = input.shift()[0];
+let answer = 0;
 let eggs = input;
 
-function dfs(now) {
-	if (now == N) {
-		let broken = 0;
-		for (let i = 0; i < N; i++) {
-			if (eggs[i][0] <= 0) broken++;
-		}
-		answer = Math.max(broken, answer);
-		return;
-	}
-	let flag = true;
-	for (let next = 0; next < N; next++) {
-		if (next == now) continue;
-		if (eggs[now][0] <= 0 || eggs[next][0] <= 0) continue;
-		flag = false;
-		eggs[now][0] = eggs[now][0] - eggs[next][1];
-		eggs[next][0] = eggs[next][0] - eggs[now][1];
-		dfs(now + 1);
-		eggs[now][0] = eggs[now][0] + eggs[next][1];
-		eggs[next][0] = eggs[next][0] + eggs[now][1];
-	}
-	if (flag) dfs(now + 1)
+function hitEgg(index) {
+    if(index == totalEgg) {
+        let brokenEggs = 0;
+        for(let i = 0;i < totalEgg;i++) {
+            if(eggs[i][0] <= 0) brokenEggs++;
+        }
+        answer = Math.max(answer, brokenEggs);
+        return;
+    }
+
+    let flag = true;
+    for(let i = 0;i < totalEgg;i++) {
+        if(i == index) continue;
+        if(eggs[index][0] <= 0 || eggs[i][0] <= 0) continue;
+        
+        flag = false;
+        eggs[index][0] -= eggs[i][1];
+        eggs[i][0] -= eggs[index][1];
+        hitEgg(index + 1);
+        eggs[index][0] += eggs[i][1];
+        eggs[i][0] += eggs[index][1];
+    }
+    if(flag) hitEgg(index + 1);
 }
 
-dfs(0)
-console.log(answer)
+hitEgg(0);
+
+console.log(answer);
