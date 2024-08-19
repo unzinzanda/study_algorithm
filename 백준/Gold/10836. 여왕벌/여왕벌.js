82d4a6a9eae258356a3030a5923dcdfbee5dfb54
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2024-08-20 00:39:04 +0900

    [Gold III] Title: 여왕벌, Time: 7276 ms, Memory: 219740 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.js
+++ 백준/Gold/10836. 여왕벌/여왕벌.js
@@ -1,3 +1,8 @@
+/**
+ * 애벌레의 성장 정도 : 왼위, 왼, 위 중 가장 많이 성장한 만큼
+ * 이 때, 주어진 가장자리 성장도는 오름차순으로 주어지기 때문에 항상 위가 선택되고
+ * 결국 가장자리를 제외하고 나머지 모든 열은 가장 윗 행의 값을 가진다
+ */
 const fs = require('fs')
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt'
 let input = fs
@@ -7,46 +12,26 @@ let input = fs
   .split('\n')
   .map((el) => el.split(' ').map(Number))
 
-const [N, M] = input.shift()
+const [M, N] = input.shift()
 
-const answer = Array.from(Array(N), () => Array(N).fill(1))
+const answer = Array(2 * M - 1).fill(1)
 
-function transInput(arr) {
-  let temp = []
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < arr[i]; j++) temp.push(i)
-  }
+for (let i = 0; i < N; i++) {
+  let idx = input[i][0]
 
-  return temp
+  for (let j = 0; j < input[i][1]; j++) answer[idx++] += 1
+  for (let j = 0; j < input[i][2]; j++) answer[idx++] += 2
 }
 
-for (let i = 0; i < M; i++) {
-  const selfGrowth = transInput(input[i])
+let result = Array(M)
 
-  // 0행 0열 성장
-  let idx = 0
-  for (let j = N - 1; j >= 0; j--) {
-    if (selfGrowth[idx] === 0) {
-      idx += 1
-      continue
-    }
-    answer[j][0] += selfGrowth[idx++]
-  }
-  for (let j = 1; j < N; j++) {
-    if (selfGrowth[idx] === 0) {
-      idx += 1
-      continue
-    }
-    answer[0][j] += selfGrowth[idx++]
-  }
-}
+result = answer.slice(M - 1, 2 * M)
 
-for (let i = 1; i < N; i++) {
-  for (let j = 1; j < N; j++) {
-    answer[i][j] = answer[0][j]
-  }
-}
+console.log(result.toString().replaceAll(',', ' '))
 
-for (let i = 0; i < answer.length; i++) {
-  console.log(answer[i].toString().replaceAll(',', ' '))
+let idx = M - 2
+
+for (let i = 1; i < M; i++) {
+  result[0] = answer[idx--]
+  console.log(result.toString().replaceAll(',', ' '))
 }
