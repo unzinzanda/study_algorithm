dc0d9d3da868bf2f9cb9c43b46152637685238e8
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2024-10-21 23:44:15 +0900

    [level 1] Title: 폰켓몬, Time: 0.71 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,19 +1,9 @@
 function solution(nums) {
     var answer = 0;
     
-    const map = new Map();
-    
-    for(let i = 0;i < nums.length;i++) {
-        if(!map.has(nums[i])) map.set(nums[i], 1);
-        else map.set(nums[i], map.get(nums[i]) + 1);
-    }
+    const set = new Set([...nums]);
     
     const r = nums.length / 2;
     
-    if(map.size > r) answer = r;
-    else {
-        answer = map.size;
-    }
-    
-    return answer;
+    return set.size > r ? r : set.size;
 }
