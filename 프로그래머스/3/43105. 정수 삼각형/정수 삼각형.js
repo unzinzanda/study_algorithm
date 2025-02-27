b6b7f1bdc871f56912e32be7cd17d5342457bbbf
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2025-02-27 23:47:58 +0900

    [level 3] Title: 정수 삼각형, Time: 26.60 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,11 @@
+function solution(triangle) {
+    let answer = 0;
+    
+    for(let i = triangle.length - 1;i >= 0;i--) {
+        for(let j = 0;j < triangle[i].length - 1;j++) {
+            triangle[i - 1][j] += Math.max(triangle[i][j], triangle[i][j + 1])
+        }
+    }
+    
+    return triangle[0][0]
+}
