7c4a95f144f7ecc9835cb0298a4f5e78d52ebceb
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2025-02-28 00:36:49 +0900

    [level 3] Title: 최고의 집합, Time: 0.33 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -1,13 +1,18 @@
+/*
+    각 원소의 곱이 최대인 경우
+    ->  s / n 으로 구성된 집합
+        만약, 나머지가 있다면 나머지만큼의 원소에 +1 해야 곱이 최대가 됨
+*/
 function solution(n, s) {
     const sum = Math.floor(s / n)
-    let answer = Array(n).fill(sum)
-    const remainder = s % n
     
-    for(let i = 1;i <= remainder;i++) {
-        answer[n - i] += 1
+    if(sum === 0) return [-1]
+    else {
+        const answer = Array(n).fill(sum)
+        const remainder = s % n
+        for(let i = 1;i <= remainder;i++) {
+            answer[n - i] += 1
+        }
+        return answer;
     }
-    
-    if(answer[0] === 0) answer = [-1]
-    
-    return answer;
 }
