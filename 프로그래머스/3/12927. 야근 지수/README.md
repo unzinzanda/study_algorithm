bf1b83395c827acab8064e500f0080f701c973b9
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2025-03-01 01:55:28 +0900

    [level 3] Title: 야근 지수, Time: 161.65 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/README.md
+++ 프로그래머스/3/12927. 야근 지수/README.md
@@ -1,10 +1,10 @@
 # [level 3] 야근 지수 - 12927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 158.86 ms
+메모리: 38.7 MB, 시간: 161.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 01일 01:54:22
+2025년 03월 01일 01:55:27
 
 ### 문제 설명
 
--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -3,13 +3,9 @@ class MaxHeap {
         this.heap = [null]
     }
     
-    size() {
-        return this.heap.length - 1
-    }
-    
     push(value) {
         this.heap.push(value)
-        let current = this.size()
+        let current = this.heap.length - 1
         
         // value가 꼭대기에 올라오게 되면 종료
         while(current !== 1) {
@@ -29,7 +25,7 @@ class MaxHeap {
             // 가장 끝 값을 맨 위로(top-down)
             this.heap[1] = this.heap.pop()
             let current = 1
-            while(current < this.size()) {
+            while(current < this.heap.length - 1) {
                 const leftChild = 2 * current
                 const rightChild = 2 * current + 1
                 
@@ -52,7 +48,7 @@ function solution(n, works) {
     const maxHeap = new MaxHeap()
     works.forEach(el => maxHeap.push(el))
     
-    while(n > 0 && maxHeap.size() > 0) {
+    while(n > 0 && maxHeap.heap.length - 1 > 0) {
         const maxValue = maxHeap.pop()
         if(maxValue !== 0) {
             maxHeap.push(maxValue - 1)
