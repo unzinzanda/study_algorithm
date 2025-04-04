eaf1028a1bbd2e8894e88e4489ea44d0953ba635
unzinzanda <93910197+unzinzanda@users.noreply.github.com>
2025-04-04 15:59:39 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 256 ms, Memory: 13392 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt'
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((el) => el.split(' '))
+
+let [str] = input.shift()
+str = str.trim()
+const [N] = input.shift()
+
+// 구간합 배열 만들기
+const arr = Array.from(Array(26), () => Array(str.length).fill(0))
+
+for (let i = 0; i < 26; i++) {
+  const alpha = String.fromCharCode(i + 'a'.charCodeAt())
+
+  if (str[0] === alpha) arr[i][0] = 1
+
+  for (let j = 1; j < str.length; j++) {
+    arr[i][j] = str[j] === alpha ? arr[i][j - 1] + 1 : arr[i][j - 1]
+  }
+}
+
+input.forEach((el) => {
+  const ascii = el[0].charCodeAt() - 'a'.charCodeAt()
+  console.log(
+    Number(el[1]) === 0
+      ? arr[ascii][Number(el[2])]
+      : arr[ascii][Number(el[2])] - arr[ascii][Number(el[1]) - 1],
+  )
+})
